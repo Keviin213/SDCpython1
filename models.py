d234bef1eaefa58d45bf3d745897ade427714dc5
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-05-14 19:21:28 -0400

    removed key

--- models.py
+++ models.py
@@ -2,6 +2,6 @@ import os.path
 
 from azure.cognitiveservices.vision.computervision import ComputerVisionAPI
 
-SUBSCRIPTION_KEY_ENV_NAME = "3af0e70b4f3641689f254019214efeda"
+SUBSCRIPTION_KEY_ENV_NAME = "tisss"
 COMPUTERVISION_LOCATION = os.environ.get("COMPUTERVISION_LOCATION", "westcentralus")
 
