b9f90300330399eed753d4146c28b4ff0d45d779
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-05-14 21:30:15 -0400

    modified about response

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -7,7 +9,10 @@ def index():
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+  req = requests.get('https://github.com/timeline.json')
+  treq = req.url 
+  resp = req.json()
+  return render_template("about.html", url=treq, result=resp)
 
 if __name__ == '__main__':
   app.run()
--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 import os.path
 
-from azure.cognitiveservices.vision.computervision import ComputerVisionAPI
 
 SUBSCRIPTION_KEY_ENV_NAME = "tisss"
 COMPUTERVISION_LOCATION = os.environ.get("COMPUTERVISION_LOCATION", "westcentralus")
