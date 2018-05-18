14eccb6499a304fc35a860b163df2a27159bef55
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-05-18 09:29:54 -0400

    changed main

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ app = Flask(__name__)
 def index():
   return render_template("index.html") 
 
+@app.route("/about")
+def about():
+  req = requests.get('https://github.com/timeline.json')
+  treq = req.url 
+  resp = req.json()
+  return render_template("about.html", url=treq, result=resp)
+
 if __name__ == '__main__':
   app.run()
