4d9c91b9f2a2889d6d2e7681b0f393c7299b166a
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-05-20 12:06:08 -0400

    added vision

--- main.py
+++ main.py
@@ -13,5 +13,19 @@ def about():
   resp = req.json()
   return render_template("about.html", url=treq, result=resp)
 
+@app.route("/vision")
+def vision():
+
+  region = 'westcentralus' #Here you enter the region of your subscription
+  url = 'https://{}.api.cognitive.microsoft.com/vision/v1.0/analyze'.format(region)
+  key = "3b290a5c45cf4f76aa61e5fb77347a07"
+  maxNumRetries = 1
+
+#  vreq = req.url 
+#  vresp = req.json()
+  vreq = "test.url"
+  vresp = "{test response}"
+  return render_template("vision.html", url=vreq, result=vresp)
+
 if __name__ == '__main__':
   app.run()
