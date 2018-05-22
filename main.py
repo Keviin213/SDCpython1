48750a2ceddeeaed2d1eee3cd5c69d934435c284
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-05-22 09:53:16 -0400

    fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from flask import Flask, render_template
+from pprint import pprint 
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,18 +17,32 @@ def about():
 @app.route("/vision")
 def vision():
 
+#
+#  set the region to use and the url/resource_path for the API nethond
+#
+#  set the subscription key from Azure Cognitive Services Resource Group
+#
   region = 'westcentralus' #Here you enter the region of your subscription
   url = 'https://{}.api.cognitive.microsoft.com/vision/v1.0/analyze'.format(region)
-  key = "bsbsbs"
+  key = "bsbsbsbsbss"
+#
+#  picture that will be sent for processing
+#  located in the img directory
+#   under  static/img 
+  pic = "static/img/people.jpg"
   maxNumRetries = 1
 
-  pathToFileInDisk = r'static/img/cut.jpg'
+#
+# read the file from local disc
+#
+  pathToFileInDisk = pic
   with open( pathToFileInDisk, 'rb' ) as f:
     data = f.read()
 
 # Computer Vision parameters
   params = { 'visualFeatures' : 'categories,tags,description,faces'}
 
+# Computer Vision header fields
   headers = dict()
   headers['Ocp-Apim-Subscription-Key'] = key
   headers['Content-Type'] = 'application/octet-stream'
@@ -36,10 +51,9 @@ def vision():
   response = requests.request( 'post', url, json = json, data = data, headers = headers, params = params )
 
   vreq = response.url 
-  vresp = response.json()
-#  vreq = "test.url"
-#  vresp = "{test response}"
-  return render_template("vision.html", url=vreq, result=vresp)
+  vresp = response.text
+
+  return render_template("vision.html", url=vreq, result=vresp, pic=pic)
 
 if __name__ == '__main__':
   app.run()
